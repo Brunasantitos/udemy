df9abc0379fb0d42304c87a4d381ae1bef37fcfa
Brunasantitos <santitos@outlook.com.br>
2022-11-28 20:50:41 -0300

    assunto: tipo de dados em js

---
+++ JS/4_6_tipos_de_dados/tipos_de_dados.js
@@ -0,0 +1,27 @@
+//tipos de dados:
+
+// number, podendo ser: + , - ou NaN(quando o dado não em numeros)
+
+var numero = 4;
+console.log(numero);
+
+var float = 5.8;
+console.log(float);
+
+//string:
+
+//pode ser escrita com aspas duplas ou sinmples
+
+var string = "Bruna";
+console.log(string);
+
+//para escrever na tela entre aspas, se usa:
+
+document.write("Olá");
+
+document.write("olá: 'bruna' ");
+
+//booleano: verdadeiro ou falso, comparacao
+
+var verdadeiro = true;
+console.log(verdadeiro);
