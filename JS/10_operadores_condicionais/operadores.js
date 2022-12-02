95977cc38b9d572562bcfa4c0a253363a6a5ae11
Brunasantitos <santitos@outlook.com.br>
2022-12-02 13:29:34 -0300

    operadores relacionais: sua estrutura, como utilizar

---
+++ JS/10_operadores_condicionais/operadores.js
@@ -0,0 +1,17 @@
+//condição: if() >> a condição fica dentro do parênteses
+
+if (true){
+    console.log("Testanto o if");
+}
+
+//testando uma mini operação:
+
+var idade = 18;
+
+if(idade > 17) {
+    console.log("Você já é maior de idade!");
+}
+
+if(idade < 19) {
+    console.log("você ainda não é maior de idade!");
+}
