d0bb8e00398642166ee43c3f98a19be18dbd7a68
Brunasantitos <santitos@outlook.com.br>
2022-12-05 20:00:19 -0300

    utilização do: is, else if e else

---
+++ JS/12_else_elif/else_elif.js
@@ -0,0 +1,11 @@
+// a funcionalidade do IF, ELSE IF (ELIF) E ELSEA, mesma lógica de Python
+
+var nome = 'Bruna';
+
+if(nome == 'Bruna'){
+    console.log("Esse é seu nome");
+} else if(nome == 'Bruna'){
+    console.log("Seu nome é Bruna");
+} else{
+    console.log("Seu nome com teste");
+}
