adf3ac66a415562b7ee02cda14f2e65ac2a70a96
Brunasantitos <santitos@outlook.com.br>
2022-12-05 22:37:45 -0300

    Comparacao: tipo e valor de dados

---
+++ JS/13_comparacao/comparacao_tipo_valor.js
@@ -0,0 +1,15 @@
+//Comparacao: tipo e valor de dados
+
+// === : verifica o valor(tipo) se é number, float, string, etc...
+
+var numero = '5';
+// se coloca o parametro diferente, considera-se o parametro como falso
+if(numero === '6'){
+    console.log("MOstra o tipo de parametro");
+}
+
+// logica considerando o valor como falso:
+
+if(numero !== 7){
+    console.log("Testando a diferena de dado");
+}
