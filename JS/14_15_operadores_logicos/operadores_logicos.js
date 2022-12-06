06476f3b2636d1b2271b6b00aceec8ad1576b83d
Brunasantitos <santitos@outlook.com.br>
2022-12-05 22:57:50 -0300

    Operadores logicos: sintaxe e como utiliza-los(or, and)

---
+++ JS/14_15_operadores_logicos/operadores_logicos.js
@@ -0,0 +1,20 @@
+/* Operdaores logicos: &&(and), retorna true apenas se as 
+duas expressoes retornarem true */
+
+var numero = 8;
+var numero_2 = 9;
+
+if (numero == 8 && numero_2 == 9){
+    console.log("testando operadores logicos");
+}
+
+/* a sintaxe também pode ser:
+    if((priorizar um calculo)){
+        console.log()]
+    }
+*/
+
+/* Operador logico: ||(or), ele retorna true caso uma das
+operacoes retorne verdadeiro */
+
+ 
