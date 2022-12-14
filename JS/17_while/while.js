cad020de588691fb2fafe599e84a578677c6621c
Brunasantitos <santitos@outlook.com.br>
2022-12-14 12:58:25 -0300

    while: sintaxe, como utiliza lo.

---
+++ JS/17_while/while.js
@@ -0,0 +1,36 @@
+/*
+
+var x = 0;
+
+while(x <= 5) {
+
+  console.log("Testando repetição " + x);
+
+  // incremetador
+  x++ // faz com que a logica não fique em loop infinito
+
+}
+*/
+var arr = ['teste', 'testando', 'a', 'b', '7']; // logica para passar o nome completo
+var y = 0;
+
+while(y <= 3) {
+
+  console.log(arr[y] + y);
+
+  y++;
+
+}
+
+/*
+var palavra = "Matheus789"; // logica para passar por cada letra da palavra
+var i = 0; // o numero significa que ele vai imprimir a partir do caracter 3
+
+while(i <= 6) {
+
+  console.log(palavra[i]);
+
+  i+=1; // o numero significa as casas a direita o codigo vai ex n2: significa que ele imprime de 2 em 2 letras
+
+}
+*/
