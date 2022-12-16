2c6ca7a63a7b71c67f359a51995068d6d6b29224
Brunasantitos <santitos@outlook.com.br>
2022-12-16 13:42:41 -0300

    lógica do while: como utiliza-lo, sua sintaxe.(o exemplo de utilização não foi adicionado)

--- udemy/python/1.iniciando_programacao/5.iterando_strings_while.py
+++ udemy/python/1.iniciando_programacao/5.iterando_strings_while.py
@@ -1,1 +1,24 @@
+
+frase = "meu nome é bruna"
+tamanho_frase = len(frase)
+
+indice = 0
+
+
 '''
+após o WHILE deve-se colocar a lógica, e assim como js deve haver o CONTADOR de acordo com a lógica
+'''
+
+while indice < 10: 
+    
+    print(frase[indice], indice)
+    
+    indice += 1 #esse no caso é o conntador para a lógica de WHILE
+                #o contador serve para o loop não ficar infinito
+
+
+        
+    
+    
+    
+    
